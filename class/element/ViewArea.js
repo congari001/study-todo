4ea96587fc63adf3010b52aedfe59e9caf09f13d
congari001 <reminosce+tw.programmer@gmail.com>
2020-06-07 19:17:47 +0900

    テスト用のなんちゃってtodoManager作成 viewArea作成 ついでにtest1.htmlの謎のスペース削除・・・

---
+++ class/element/ViewArea.js
@@ -0,0 +1,10 @@
+class ClassViewArea extends ClassElement {
+    constructor(planList, doneList) {
+        super('div');
+        this._view_area_assets = {};
+        this._view_area_assets.planList = planList;
+        this._view_area_assets.doneList = doneList;
+        this.appendChild(this._view_area_assets.planList);
+        this.appendChild(this._view_area_assets.doneList);
+    }
+}
